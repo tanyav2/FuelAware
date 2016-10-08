9e8ea9434ae71b3bb3882b93812bd009edf8def8
Tanya Verma <tanyav2@illinois.edu>
2016-10-08 04:27:11 -0400

    UI done

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,14 @@
 // Your code goes here
 function myFunction(){
         x = document.getElementById("myText");
+        https://maps.googleapis.com/maps/api/place/queryautocomplete/json?key=AIzaSyAjDGacx8XJdRlhWYCM_BfKKuVUslrsL8M&input=x.value
         console.log(x.value);
       }
 
-      function myFunction2(){
-       }
+    
+function autoComplete(el){
+	console.log("kek");
+
+	var x = document.createElement("div")
+	document.appendChild(x)
+}
