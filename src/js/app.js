0eddf3e255c5d9efe540dfdeb47e47ec170136dc
ArkinDharawat <arkin.dharawat@gmail.com>
2016-10-08 03:05:14 -0400

    third commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 // Your code goes here
 function myFunction(){
         x = document.getElementById("myText");
-        console.log(x.value);
+        console.log(`string text ${x.value} string text`);
       }
 
       function myFunction2(){
