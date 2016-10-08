fcdf67caa311a54dc55daef4e2d313d2b6cdbbbb
ArkinDharawat <arkin.dharawat@gmail.com>
2016-10-08 02:42:25 -0400

    second commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,8 @@
 // Your code goes here
-
-var vinElem = document.getElementById('vin');
-gm.info.getVehicleConfiguration(function(data) {
-  vinElem.innerHTML = gm.info.getVIN();
-});
-
-    function myFunction(){
+function myFunction(){
         x = document.getElementById("myText");
         console.log(x.value);
       }
-    
+
       function myFunction2(){
        }
